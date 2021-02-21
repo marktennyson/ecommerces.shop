877950575a05f8de9c2895abc3ad85f74e500626
Mark Tennyson <46404058+marktennyson@users.noreply.github.com>
2021-02-21 17:22:10 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '999ea4debdba71390bf87f9b5f655277'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13936740-a371-4695-a26f-33e24ee667e9'
+};
